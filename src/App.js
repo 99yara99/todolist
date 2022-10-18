502d7576631a9fe3e0e6a6a5963f4781f4028fac
99yara99 <itgeryaros@gmail.com>
2022-10-18 19:37:44 +0300

    add action creator and modal window for update button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Modal from 'react-modal';
 import './App.css';
 import Form from './components/Form/Form';
 import List from './components/List/List';
-import { ADDNOTE, del, UPDATENOTE } from './redux/actions';
+import {
+  addNoteAction,
+  deleteNoteAction,
+  updateNoteAction,
+} from './redux/actions';
 
 function App() {
   const [inputText, setInputText] = useState('');
@@ -14,27 +19,30 @@ function App() {
   const addNote = (event) => {
     event.preventDefault();
     if (inputText !== '') {
-      dispatch({ type: ADDNOTE, payload: inputText });
+      dispatch(addNoteAction(inputText));
     }
     setInputText('');
   };
 
   const note = useSelector((state) => state.todos);
 
-  const deleteNote = (id) => {
-    dispatch(del(id));
-  };
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [inputTextModal, setInputTextModal] = useState('');
+  const handleChangeModal = (event) => setInputTextModal(event.target.value);
 
-  const updateNote = (id) => {
-    let updateNote = prompt('');
+  const deleteNote = (id) => dispatch(deleteNoteAction(id));
 
-    dispatch({
-      type: UPDATENOTE,
-      payload: {
-        id: id,
-        text: updateNote,
-      },
-    });
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const updateNote = (event, id) => {
+    event.preventDefault();
+    dispatch(updateNoteAction(id, inputTextModal));
+    closeModal();
   };
 
   let listOfNotes = note.map((note) => (
@@ -47,13 +55,24 @@ function App() {
       >
         x
       </button>
-      <button
-        className="updateButton"
-        type="button"
-        onClick={() => updateNote(note.id)}
-      >
+      <button className="updateButton" type="button" onClick={openModal}>
         Редагувати
       </button>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+        <form
+          id="todoFormModal"
+          onSubmit={(event) => updateNote(event, note.id)}
+        >
+          <input
+            type="text"
+            placeholder={note.text}
+            value={inputTextModal}
+            onChange={handleChangeModal}
+          />
+          <button type="submit">Редагувати</button>
+        </form>
+      </Modal>
     </li>
   ));
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-// import thunk from 'redux-thunk';
+import { createAction } from '@reduxjs/toolkit';
 export const ADDNOTE = 'ADD_NOTE';
 export const DELETENOTE = 'DELETE_NOTE';
 export const UPDATENOTE = 'UPDATE_NOTE';
@@ -9,13 +9,23 @@ export const UPDATENOTE = 'UPDATE_NOTE';
 //       payload: inputText,
 //     };
 // }
-
-export function del(id) {
-  return {
-    type: DELETENOTE,
-    payload: id,
-  };
-}
+export const addNoteAction = createAction(ADDNOTE);
+export const deleteNoteAction = createAction(DELETENOTE);
+export const updateNoteAction = createAction(
+  UPDATENOTE,
+  function prepare(id, inputTextModal) {
+    return {
+      payload: {
+        id: id,
+        text: inputTextModal,
+      },
+    };
+  }
+);
+//   return {
+//     type: DELETENOTE,
+//     payload: id,
+//   }
 
 // export function updateNote() {
 //   return {
